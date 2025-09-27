1250c56a0cabb67f3f9619201018e2e338ce499a
Cowther Richmond <richmondcowther@gmail.com>
2025-09-27 21:50:04 +0000

    #Update

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,10 +1,23 @@
 import { AboutProps } from "@/interfaces";
+import Button from "@/components/common/Button";
 import React from "react";
 
 const About: React.FC<AboutProps> = ({ title="" }) => {
+    const handleSmallClick = () => alert("Small button clicked!");
+    const handleMediumClick = () => alert("Medium button clicked!");
+    const handleLargeClick = () => alert("Large button clicked!");
+
         return (
-        <div>
-            <h1 className="flex font-bold text-xl justify-center items-center p-10">Find everything about me here!</h1>
+        <div className="">
+            <h1 className="flex font-bold text-2xl justify-center items-center mb-4">
+                Find everything about me here!
+            </h1>
+
+           <div className="flex flex-col gap-4">
+                <Button label="About Me" size="small" shape="rounded-sm" onClick={handleSmallClick} />
+                <Button label="About Me" size="medium" shape="rounded-md" onClick={handleMediumClick} />
+                <Button label="About Me" size="large" shape="rounded-full" onClick={handleLargeClick} />
+           </div>
         </div>
     );
 }
